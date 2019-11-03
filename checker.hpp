1ce05bf865d049230e7fb5d6ddf3881afc795120
Vadim Piven <vadim@piven.tech>
2019-11-03 13:23:30 +0300

    multithread reorganised

--- checker.hpp
+++ checker.hpp
@@ -6,29 +6,27 @@
 
 template<uint32_t P>
 class checker {
-    polynomialgf<P> poly;
-
-    pthread_mutex_t *mutex;
-    pthread_cond_t *cond;
-
-    bool _busy;
 public:
     struct result_type {
         bool irreducible;
         bool primitive;
     };
 
-private:
-    result_type res;
-
-public:
     enum method {
         berlekamp = 0,
         rabin
     };
 
 private:
-    const method _method;
+    polynomialgf<P> poly;
+
+    pthread_mutex_t *mutex;
+    pthread_cond_t *cond;
+
+    bool _busy;
+    bool _terminate;
+    result_type res;
+    method _method;
 
 public:
     explicit
@@ -44,13 +42,15 @@ public:
     [[nodiscard]]
     bool busy() const noexcept;
 
+    void terminate() noexcept;
+
     [[nodiscard]]
     const result_type &result() const noexcept;
 };
 
 template<uint32_t P>
 checker<P>::checker(pthread_mutex_t *mutex, pthread_cond_t *cond, const method meth) noexcept :
-        poly(), mutex(mutex), cond(cond), _busy(false), res({false, false}), _method(meth) {}
+        poly(), mutex(mutex), cond(cond), _busy(false), res({false, false}), _method(meth), _terminate(false) {}
 
 template<uint32_t P>
 polynomialgf<P> checker<P>::get() const {
@@ -59,41 +59,47 @@ polynomialgf<P> checker<P>::get() const {
 
 template<uint32_t P>
 void checker<P>::set(polynomialgf<P> val) {
-    _busy = true;
-    res = { false, false };
     poly = val;
+    _busy = true;
 }
 
 template<uint32_t P>
 void *checker<P>::check(void *arg) noexcept {
     auto *c = static_cast<checker *>(arg);
 
-    switch (c->_method) {
-        case method::rabin:
-            c->res.irreducible = is_irreducible_rabin(c->poly);
-            break;
-        default: // method::berlekamp
-            c->res.irreducible = is_irreducible_berlekamp(c->poly);
-            break;
-    }
-    if (c->res.irreducible) {
-        c->res.primitive = is_primitive(c->poly);
-    }
+    while (true) {
+        if (c->_terminate) { pthread_exit(nullptr); }
+        if (!c->_busy) { continue; }
+        switch (c->_method) {
+            case method::rabin:
+                c->res.irreducible = is_irreducible_rabin(c->poly);
+                break;
+            default: // method::berlekamp
+                c->res.irreducible = is_irreducible_berlekamp(c->poly);
+                break;
+        }
+        c->res.primitive = c->res.irreducible ? is_primitive(c->poly) : false;
 
-    pthread_mutex_lock(c->mutex);
-    c->_busy = false;
-    pthread_cond_signal(c->cond);
-    pthread_mutex_unlock(c->mutex);
-
-    pthread_exit(nullptr);
+        pthread_mutex_lock(c->mutex);
+        c->_busy = false;
+        pthread_cond_signal(c->cond);
+        pthread_mutex_unlock(c->mutex);
+    }
 }
 
 template<uint32_t P>
+[[nodiscard]]
 bool checker<P>::busy() const noexcept {
     return _busy;
 }
 
 template<uint32_t P>
+void checker<P>::terminate() noexcept {
+    _terminate = true;
+}
+
+template<uint32_t P>
+[[nodiscard]]
 const typename checker<P>::result_type &checker<P>::result() const noexcept {
     return res;
 }
--- main.cpp
+++ main.cpp
@@ -8,10 +8,11 @@
 
 template<uint32_t P>
 [[nodiscard]]
-polynomialgf<P> generate_primitive(typename polynomialgf<P>::size_type degree, const typename checker<P>::method meth) {
-    static const unsigned threadsNum =
-            (std::thread::hardware_concurrency() > 0 ? std::thread::hardware_concurrency() : 1);
-
+polynomialgf<P> generate_primitive(
+        typename polynomialgf<P>::size_type degree,
+        const typename checker<P>::method meth,
+        const unsigned threadsNum
+) {
     auto countBusy = [](const std::vector<checker<P>> &c) noexcept {
         unsigned n = 0;
         for (auto &checker : c) { n += checker.busy(); }
@@ -21,57 +22,74 @@ polynomialgf<P> generate_primitive(typename polynomialgf<P>::size_type degree, c
     polynomialgf<P> res; // возвращаемое значение
 
     // всё для работы с потоками
-    pthread_t threads[threadsNum];
-    pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+    pthread_mutex_t mutex;
     pthread_mutexattr_t attr;
-    pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+    pthread_cond_t cond;
 
     if (pthread_mutexattr_init(&attr) ||
         pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK) ||
         pthread_mutex_init(&mutex, &attr) ||
-        pthread_cond_init(&cond, nullptr)) { throw std::runtime_error("pthread initialisation failed"); }
+        pthread_cond_init(&cond, nullptr)) {
+        throw std::runtime_error("pthread initialisation failed");
+    }
 
-    // по одному проверщику в каждом потоке
-    auto c = std::vector<checker<P>>(threadsNum, checker<P>(&mutex, &cond, meth));
-
-    // закрываем мьютекс, таким образом изменение состояния busy() отдельных потоков
-    // смогут происходить только внутри pthread_cond_wait
+    // закрываем мьютекс, таким образом изменения состояния busy() отдельных потоков
+    // на значение false смогут происходить только внутри pthread_cond_wait
     pthread_mutex_lock(&mutex);
+
+    // по одному проверщику на каждый поток
+    auto checkers = std::vector<checker<P>>(threadsNum, checker<P>(&mutex, &cond, meth));
+    std::vector<pthread_t> threads(threadsNum);
+    pthread_attr_t thread_attr;
+    if (pthread_attr_init(&thread_attr) ||
+        pthread_attr_setdetachstate(&thread_attr, PTHREAD_CREATE_DETACHED)) {
+        throw std::runtime_error("pthread initialisation failed");
+    }
+    for (unsigned i = 0; i < threadsNum; ++i) {
+        // создаём новый поток для выполнения проверки
+        pthread_create(&threads[i], nullptr, &checker<P>::check, &checkers[i]);
+    }
+
     while (true) {
-        if (countBusy(c) == threadsNum) {
+        if (countBusy(checkers) == threadsNum) {
             // ждём изменения числа занятых потоков
             pthread_cond_wait(&cond, &mutex);
         }
 
-        for (unsigned j = 0; j < threadsNum; ++j) {
+        for (unsigned i = 0; i < threadsNum; ++i) {
             // находим первый свободный поток
-            if (c[j].busy()) { continue; }
-            if (c[j].result().primitive) {
-                res = c[j].get();
+            if (checkers[i].busy()) { continue; }
+            if (checkers[i].result().primitive) {
+                res = checkers[i].get();
                 goto END;
             }
             // генерируем случайный многочлен для проверки
-            c[j].set(random<P>(degree));
-            // создаём новый поток для выполнения проверки
-            pthread_create(&threads[j], nullptr, &checker<P>::check, &c[j]);
-            // отсоединеняем поток
-            pthread_detach(threads[j]);
+            checkers[i].set(random<P>(degree));
         }
     }
     END:
-    // ждём завершения всех созданных потоков
-    while (countBusy(c)) {
+    // сообщаем всем потокам, что они должны прекратить работу
+    for (auto &c : checkers) {
+        c.terminate();
+    }
+    // ждём завершения всех потоков
+    while (countBusy(checkers)) {
         pthread_cond_wait(&cond, &mutex);
     }
     pthread_mutex_unlock(&mutex);
-    if (pthread_cond_destroy(&cond) || pthread_mutex_destroy(&mutex)
-        || pthread_mutexattr_destroy(&attr)) { throw std::runtime_error("pthread destruction failed"); }
+    if (pthread_cond_destroy(&cond) ||
+        pthread_mutex_destroy(&mutex) ||
+        pthread_mutexattr_destroy(&attr) ||
+        pthread_attr_destroy(&thread_attr)) {
+        throw std::runtime_error("pthread destruction failed");
+    }
     return res;
 }
 
 int main() {
     const uint32_t P = 2;
-    std::cout << generate_primitive<P>(5, checker<P>::method::rabin) << std::endl;
+    const unsigned threadsNum = std::thread::hardware_concurrency() - 1;
+    std::cout << generate_primitive<P>(5, checker<P>::method::rabin, threadsNum) << std::endl;
 
     return 0;
 }
