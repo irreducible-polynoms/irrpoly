286e928630511fc53a624d9729c78f8a500ec69a
Vadim Piven <vadim@piven.tech>
2019-11-08 21:14:03 +0300

    fixed authors

--- checker.hpp
+++ checker.hpp
@@ -1,6 +1,6 @@
 /**
  * @file    checker.hpp
- * @author  Vadim Piven <vadim@piven.tech>, Zimin Fedor <zimfv@yandex.ru>
+ * @author  Vadim Piven <vadim@piven.tech>
  * @license Free use of this library is permitted under the
  * guidelines and in accordance with the MIT License (MIT).
  * @url     https://github.com/irreducible-polynoms/irrpoly
