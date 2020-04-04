234f0f37083b093849d5cfe0ca46bf97a97980c1
Vadim Piven <vadim@piven.tech>
2020-04-04 18:15:26 +0300

    version incremented

--- docs/html/menudata.js
+++ docs/html/menudata.js
@@ -21,13 +21,4 @@ with this program; if not, write to the Free Software Foundation, Inc.,
 for the JavaScript code in this file
 */
 var menudata={children:[
-{text:"Титульная страница",url:"index.html"},
-{text:"Классы",url:"annotated.html",children:[
-{text:"Классы",url:"annotated.html"},
-{text:"Алфавитный указатель классов",url:"classes.html"},
-{text:"Члены классов",url:"functions.html",children:[
-{text:"Указатель",url:"functions.html"},
-{text:"Функции",url:"functions_func.html"},
-{text:"Определения типов",url:"functions_type.html"}]}]},
-{text:"Файлы",url:"files.html",children:[
-{text:"Файлы",url:"files.html"}]}]}
+{text:"Титульная страница",url:"index.html"}]}
--- docs/html/search/searchdata.js
+++ docs/html/search/searchdata.js
@@ -1,30 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "cdgilmnoprsuwx~",
-  1: "cgmnprs",
-  2: "cgp",
-  3: "cdgilmorsuwx~",
-  4: "cgip",
-  5: "ip"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "files",
-  3: "functions",
-  4: "typedefs",
-  5: "enums"
 };
 
 var indexSectionLabels =
 {
-  0: "Указатель",
-  1: "Классы",
-  2: "Файлы",
-  3: "Функции",
-  4: "Определения типов",
-  5: "Перечисления"
 };
 
