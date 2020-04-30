5923dd296c9f9d3b44ac217e1e585352d73bd1ee
Vadim Piven <vadim@piven.tech>
2020-04-30 21:39:02 +0300

    shortened

--- tests/wolfram/main.cpp
+++ tests/wolfram/main.cpp
@@ -6,13 +6,14 @@ using namespace irrpoly;
 
 auto main() -> int {
     std::cout << "TEST DATA" << std::endl;
+    std::cout << std::noboolalpha;
     for (auto base : {2, 3, 5, 7}) {
         auto gf = make_gf(base);
         for (size_t i = 4; i < 10; ++i) {
             for (size_t j = 0; j < i / 2; ++j) {
                 gfpoly poly = gfpoly::random(gf, i);
-                std::cout << (is_irreducible_benor(poly) ? '1' : '0') << " "
-                          << (is_primitive_definition(poly) ? '1' : '0') << " "
+                std::cout << is_irreducible_benor(poly) << " "
+                          << is_primitive_definition(poly) << " "
                           << base << " " << poly << std::endl;
             }
         }
